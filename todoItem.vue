7fe0b493b6667b5601ac533519f6d05664a69e09
Gopikrishna-Gurivisetti <137783366+Gopikrishna-Gurivisetti@users.noreply.github.com>
2023-06-27 20:16:41 +0530

    Add files via upload
    first commit

---
+++ todoItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+<li v-for="todo in props.todos" :key="todo">
+    <span>{{ todo.text }}</span>
+</li>
+    </div>
+</template>
+<script setup>
+
+const props=defineProps(['todos'])
+
+</script>
